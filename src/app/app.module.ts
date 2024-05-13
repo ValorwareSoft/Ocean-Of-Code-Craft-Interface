6ca3f80909d7139a53a6eb2f569f2786f031c942
Valorware Soft <valorwaresoftceo@gmail.com>
2024-05-14 01:10:07 +0530

    packages installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    ToastrModule.forRoot({
+      preventDuplicates : true,
+      timeOut :2000,
+      maxOpened : 1
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
