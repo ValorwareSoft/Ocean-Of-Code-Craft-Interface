4ac34df0692d4e31c7bad84eeab2c49956eac1d7
Valorware Soft <valorwaresoftceo@gmail.com>
2024-05-16 11:35:03 +0530

    navbar changes

--- src/app/modules/navbar-module/navbar/navbar.component.ts
+++ src/app/modules/navbar-module/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 import { ThemeService } from 'src/app/services/theme-service/theme.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-navbar',
@@ -11,21 +12,30 @@ import { ThemeService } from 'src/app/services/theme-service/theme.service';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-  constructor(private router: Router, public themeService : ThemeService) {}
-
+  constructor(
+    private router: Router,
+    public themeService: ThemeService,
+    private toastr: ToastrService
+  ) { }
+ 
+  login: boolean = false;
   navigate(link: string) {
     switch (link) {
       case 'home':
         this.router.navigate(['/home']);
         break;
-      case 'about us':
+      case 'about-us':
         this.router.navigate(['/about-us']);
         break;
       case 'contact-us':
         this.router.navigate(['/contact-us']);
         break;
       case 'get-custom-code':
-        this.router.navigate(['/get-custom-code']);
+        if (this.login) {
+          this.router.navigate(['/get-custom-code']);
+        } else {
+          this.toastr.error('Please Login To Get Customised Code');
+        }
         break;
       case 'sign-up':
         this.router.navigate(['/sign-up']);
@@ -33,22 +43,21 @@ export class NavbarComponent {
       case 'login':
         this.router.navigate(['/login']);
         break;
-      case 'mode':
-        this.router.navigate(['/mode']);
-        break;
-      case 'profile':
-        this.router.navigate(['/profile']);
-        break;
-      case 'change-password':
-        this.router.navigate(['/change-password']);
-        break;
       case 'logout':
         this.router.navigate(['/home']);
         break;
     }
   }
 
-  toggleTheme(){
+  toggleTheme() {
     this.themeService.toggleDarkMode();
   }
+
+  
+  
+  
+  
+  
+  
+
 }
