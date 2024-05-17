0d2a83eaeab712c3312261c9469c7a39d2c75637
Valorware Soft <valorwaresoftceo@gmail.com>
2024-05-18 03:29:20 +0530

    about push

--- src/app/modules/navbar-module/navbar-links/about-us/about-us.component.ts
+++ src/app/modules/navbar-module/navbar-links/about-us/about-us.component.ts
@@ -9,5 +9,16 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./about-us.component.scss']
 })
 export class AboutUsComponent {
-
+  // components: string[] = [
+  //   'Login Forms', 'Sign-Up Forms', 'Headers', 'Navigation Bars', 'Side Menus',
+  //   'Charts', 'Tables', 'Popups', 'Toasters', 'Footers', 'Carousels', 'Modals',
+  //   'Accordions', 'Tooltips', 'Progress Bars', 'Tabs', 'Forms', 'Grids', 'Buttons',
+  //   'Badges', 'Alerts', 'Breadcrumbs', 'Pagination', 'Cards', 'Dropdowns',
+  //   'Jumbotrons', 'Media Objects', 'Lists', 'Spinners', 'Toast Notifications',
+  //   'Calendar Widgets', 'Image Galleries', 'Responsive Tables', 'Search Bars',
+  //   'Input Groups', 'File Uploads', 'Date Pickers', 'Sliders', 'Graphs',
+  //   'Multi-step Forms', 'Data Visualization Tools', 'Interactive Maps',
+  //   'Social Media Buttons', 'Authentication Components', 'API Integration Snippets',
+  //   'Code Snippets for Animations', 'Lazy Loading Components'
+  // ];
 }
