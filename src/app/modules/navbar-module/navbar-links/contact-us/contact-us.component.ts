cedc1ed86921b9aad6f54a2c4c2e3fcf79bff5b4
Valorware Soft <valorwaresoftceo@gmail.com>
2024-05-17 12:18:21 +0530

    custom code puhes

--- src/app/modules/navbar-module/navbar-links/contact-us/contact-us.component.ts
+++ src/app/modules/navbar-module/navbar-links/contact-us/contact-us.component.ts
@@ -1,13 +1,94 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent {
+  showDetails: boolean = false;
+  customCodeForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private toastr: ToastrService) {
+    this.customCodeForm = this.formBuilder.group({
+      firstName: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(30),
+          Validators.pattern('^[a-zA-Z ]+$'),
+        ],
+      ],
+      lastName: [
+        '',
+        [
+          Validators.minLength(2),
+          Validators.maxLength(30),
+          Validators.pattern('^[a-zA-Z ]+$'),
+        ],
+      ],
+      emailAddress: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            '^\\w+([.-]?\\w+)*@\\w+([.-]?\\w+)*(\\.\\w{2,3})+$'
+          ),
+        ],
+      ],
+      phoneNumber: ['', Validators.pattern('^[0-9]{10}$')],
+      requirement: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(20),
+          Validators.maxLength(1000),
+        ],
+      ],
+      referenceFile: [''],
+    });
+  }
+
+  referenceFile(event: any) {
+    const selectedFile = event.target.files;
+
+    if (selectedFile && selectedFile.length > 0) {
+      const fileType = selectedFile[0].type;
+
+      if (
+        fileType !== 'application/pdf' && !fileType.startsWith('image/')
+      ) {
+        this.customCodeForm
+          .get('referenceFile')
+          ?.setErrors({ invalidFile: true });
+      }
+
+      if (selectedFile[0].size > 3145728) {
+        this.customCodeForm
+          .get('referenceFile')
+          ?.setErrors({ invalidFileTypeOrSize: true });
+      }
+    }
+  }
+
+  onSubmit(): void {
+    if (this.customCodeForm.valid) {
+      this.toastr.success('Request Submitted Successfully');
+      console.log('Form values:', this.customCodeForm.value);
+    } else {
+      this.customCodeForm.markAllAsTouched();
+      this.toastr.error('Please Fill The Form Properly');
+    }
+  }
+
+  toggleDetails(): void {
+    this.showDetails = !this.showDetails;
+  }
 
 }
--- src/app/modules/navbar-module/navbar-links/get-custom-code/get-custom-code.component.ts
+++ src/app/modules/navbar-module/navbar-links/get-custom-code/get-custom-code.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import {
   FormBuilder,
   FormGroup,
-  FormsModule,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
@@ -19,53 +18,53 @@ import { ToastrService } from 'ngx-toastr';
 export class GetCustomCodeComponent {
   showDetails: boolean = false;
   customCodeForm: FormGroup;
+  fileUploaded: boolean = false;
 
-  constructor(private formBuilder: FormBuilder, private toastr : ToastrService) {
+  constructor(private formBuilder: FormBuilder, private toastr: ToastrService) {
     this.customCodeForm = this.formBuilder.group({
-      firstName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(30), Validators.pattern('^[a-zA-Z ]+$')]],
-      lastName: ['', [Validators.minLength(2), Validators.maxLength(30), Validators.pattern('^[a-zA-Z ]+$')]],
-      emailAddress: ['', [Validators.required, Validators.pattern('^\\w+([.-]?\\w+)*@\\w+([.-]?\\w+)*(\\.\\w{2,3})+$')]],
-      phoneNumber: ['', Validators.pattern('^[0-9]{10}$')],
-      requirement: ['', [Validators.required, Validators.minLength(20), Validators.maxLength(1000)]],
-      referenceFile: ['', [this.validateFileType]]
+      requirement: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(20),
+          Validators.maxLength(1000),
+        ],
+      ],
+      referenceFile: [''],
     });
   }
 
-  validateFileType(control: any): { [key: string]: any } | null {
-    const file = control.value;
-    if (file instanceof File) {
-      const allowedExtensions = ['jpg', 'jpeg', 'png', 'pdf'];
-      const fileNameParts = file.name.split('.');
-      if (fileNameParts.length === 1) {
-        return { 'invalidFile': true };
+  referenceFile(event: any) {
+    this.fileUploaded = false;
+    const selectedFile = event.target.files;
+    if (selectedFile && selectedFile.length > 0) {
+      this.fileUploaded = true;
+      const fileType = selectedFile[0].type;
+      if (fileType !== 'application/pdf' && !fileType.startsWith('image/')) {
+        this.customCodeForm
+          .get('referenceFile')
+          ?.setErrors({ invalidFile: true });
       }
-      const extension = fileNameParts.pop()?.toLowerCase();
-      if (!extension || allowedExtensions.indexOf(extension) === -1 || file.type.startsWith('video')) {
-        return { 'invalidFileTypeOrSize': true };
-      }
-  
-      const maxSize = 5 * 1024 * 1024; // 5MB in bytes
-      if (file.size > maxSize) {
-        return { 'invalidFileTypeOrSize': true };
+      if (selectedFile[0].size > 3145728) {
+        this.customCodeForm
+          .get('referenceFile')
+          ?.setErrors({ invalidFileTypeOrSize: true });
       }
     }
-    return null;
   }
-  
-  
-  
-  
-  
 
-
+  removeFile() {
+    this.customCodeForm.get('referenceFile')?.reset();
+    this.fileUploaded = false;
+  }
 
   onSubmit(): void {
     if (this.customCodeForm.valid) {
-      this.toastr.success("Request Submitted Successfully");
+      this.toastr.success('Request Submitted Successfully');
       console.log('Form values:', this.customCodeForm.value);
     } else {
       this.customCodeForm.markAllAsTouched();
-      this.toastr.error("Please Fill The Form Properly");
+      this.toastr.error('Please Fill The Form Properly');
     }
   }
 
